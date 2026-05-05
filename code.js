96a8359c567e78e9395e5867549711627593434e
daniel luke <manthid5@gmail.com>
2026-05-05 20:05:59 +0300

    Fix form submission logic and improve contact page text

--- code.js
+++ code.js
@@ -1,27 +1,22 @@
 const header = document.querySelector("header");
-
 const h1 = document.querySelector("h1");
-const button = document.querySelector("#submit-button");
-let fullName;
-let email;
-let message;
+const form = document.querySelector("form");
 
-button.addEventListener("click", (event) => {
-  // prevent form from refreshing page
+const resultParagraph = document.querySelector("#result");
+const headerParagraph = document.querySelector("#header-paragraph");
+
+form.addEventListener("click", (event) => {
   event.preventDefault();
 
-  // retreive values from the form
   const fullName = document.querySelector("#full-name").value;
   const email = document.querySelector("#email").value;
   const message = document.querySelector("#message").value;
 
-  
-  if (!fullName || email|| message) {
+  if (!fullName || !email ||!message) {
     resultParagraph.textContent = "Please fill in all fields.";
     return;
   }
 
   h1.textContent = fullName;
-  headerParagraph.textContent = message;
-  resultParagraph.textContent = "Your message has been sent succesfully";
+  resultParagraph.textContent = `Your message has been sent successfully`;
 });

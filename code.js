02e72c339e6a4f9ba08ed14ee5b49a598f5410b7
daniel luke <manthid5@gmail.com>
2026-05-05 19:07:15 +0300

    Implement contact form functionality

---
+++ code.js
@@ -0,0 +1,20 @@
+const header = document.querySelector("header");
+
+const h1 = document.querySelector("h1");
+const button = document.querySelector("#submit-button");
+let fullName;
+let email;
+let message;
+
+button.addEventListener("click", (event) => {
+  // prevent form from refreshing page
+  event.preventDefault();
+
+  // retreive values from the form
+  const fullName = document.querySelector("#full-name").value;
+  const email = document.querySelector("#email").value;
+  const message = document.querySelector("#message").value;
+
+  h1.textContent = fullName;
+  headerParagraph.textContent = message;
+});
